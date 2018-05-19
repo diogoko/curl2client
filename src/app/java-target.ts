834a17393136a83085ab8c83b9a0d5a9bb55201b
Diogo Kollross <diogoko@gmail.com>
2018-05-19 14:14:19 -0300

    java template

--- src/app/java-target.ts
+++ src/app/java-target.ts
@@ -11,7 +11,32 @@ export class JavaTarget extends Target {
   }
 
   generate(args: CurlCommand): string {
-    return 'bla';
+    var headersText = '';
+    if (args.headers.length > 0) {
+      var headersStatements = args.headers.map(h => `con.setRequestProperty("${h.name}", "${h.value}");`);
+      headersText = '\n' + headersStatements.join('\n');
+    }
+
+    var dataText = '';
+    if (args.data) {
+      dataText = `
+con.setDoOutput(true);
+con.getOutputStream().write("${args.data}".getBytes("UTF-8"));`
+    }
+
+    return `HttpURLConnection con = (HttpURLConnection) new URL("${args.url}").openConnection();
+con.setRequestMethod("${args.method}");${headersText}${dataText}
+
+int responseCode = con.getResponseCode();
+
+// Read response body as string
+ByteArrayOutputStream result = new ByteArrayOutputStream();
+byte[] buffer = new byte[8192];
+int length;
+while ((length = con.getInputStream().read(buffer)) != -1) {
+    result.write(buffer, 0, length);
+}
+string responseBody = result.toString("UTF-8");`;
   }
 
 }
