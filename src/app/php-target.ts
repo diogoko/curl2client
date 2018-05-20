e8537adc3ccece3ad44e51b123196bb77d67baaa
Diogo Kollross <diogoko@gmail.com>
2018-05-19 22:51:49 -0300

    php target

--- src/app/php-target.ts
+++ src/app/php-target.ts
@@ -11,7 +11,38 @@ export class PHPTarget extends Target {
   }
 
   generate(args: CurlCommand): string {
-    return 'bla';
+    const indentationSize = 2;
+
+    var contextFields = [];
+    contextFields.push(`'method' => '${args.method}'`);
+    contextFields.push(`'ignore_errors' => true`);
+
+    if (args.headers.length > 0) {
+      var headersItems = args.headers.map(h => `'${h.name}: ${h.value}'`);
+      var headersText = this.indent(headersItems.join(',\n'), indentationSize);
+
+      contextFields.push(`'header' => [
+${headersText}
+]`);
+    }
+
+    if (args.data) {
+      contextFields.push(`'content' => '${args.data}`);
+    }
+
+    var contextFieldsText = this.indent(contextFields.join(',\n'), indentationSize * 2);
+    return `$context = stream_context_create([
+  'http' => [
+${contextFieldsText}
+  ]
+]);
+
+$responseBody = file_get_contents('${args.url}', false, $context);
+
+// Parse response code
+preg_match('#HTTP/[0-9\\.]+\\s+([0-9]+)#', $http_response_header[0], $matches);
+$responseCode = $matches[1];
+`;
   }
 
 }
